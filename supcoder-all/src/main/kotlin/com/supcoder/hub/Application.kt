3a5345b48b246b96673220581d584688886f4700
lee <noble.lee@foxmail.com>
2024-12-10 02:19:54 +0800

    feat(hub): 解决依赖冲突

--- supcoder-all/src/main/kotlin/com/supcoder/hub/Application.kt
+++ supcoder-all/src/main/kotlin/com/supcoder/hub/Application.kt
@@ -1,5 +1,6 @@
 package com.supcoder.hub
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -8,12 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-//@SpringBootApplication(SpringBootApplication.scanBasePackages = {"com.supcoder.hub"})
-//@MapperScan("com.supcoder.hub.db.dao")
-//@EnableTransactionManagement
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["com.supcoder.hub"])
+@MapperScan("com.supcoder.hub.db.dao")
+@EnableTransactionManagement
 @EnableScheduling
-class Application
+open class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
