df20662e9a6826f838d52ed292657eead7252649
lee <noble.lee@foxmail.com>
2024-12-10 02:03:31 +0800

    feat(hub): 解决依赖冲突

--- supcoder-dashboard/src/main/kotlin/com/supcoder/hub/dashboard/controller/UserController.kt
+++ supcoder-dashboard/src/main/kotlin/com/supcoder/hub/dashboard/controller/UserController.kt
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import com.supcoder.hub.core.util.ResultUtil
+import com.supcoder.hub.db.domain.User
 
 @RestController
 @RequestMapping("/api/users")
