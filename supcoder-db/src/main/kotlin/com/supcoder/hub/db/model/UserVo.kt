1876dfc9b8baef36bbf1a8d48435c09afd470da0
lee <noble.lee@foxmail.com>
2024-12-09 22:40:54 +0800

    feat: 完成用户查询模块

---
+++ supcoder-db/src/main/kotlin/com/supcoder/hub/db/model/UserVo.kt
@@ -0,0 +1,26 @@
+package com.supcoder.hub.db.model
+
+/**
+ * UserVo
+ *
+ * @author lee
+ * @date 2024/12/9
+ */
+        data class UserVo(
+    val id: Long = 0,
+    val username: String,
+    val email: String,
+    var password: String,
+    val name: String,
+    val avatar: String? = null,
+    val signature: String? = null,
+    val title: String? = null,
+    val group: String? = null,
+    val notifyCount: Int? = null,
+    val unreadCount: Int? = null,
+    val country: String? = null,
+    val access: String? = null,
+    val address: String? = null,
+    val phone: String? = null
+)
+
--- supcoder-db/src/main/kotlin/com/supcoder/hub/db/service/UserService.kt
+++ supcoder-db/src/main/kotlin/com/supcoder/hub/db/service/UserService.kt
@@ -1,33 +1,104 @@
-package com.supcoder.core.service
+package com.supcoder.hub.db.service
 
-import com.supcoder.core.model.domain.User
-import org.springframework.beans.factory.annotation.Autowired
+import com.alibaba.druid.util.StringUtils
+import com.github.pagehelper.PageHelper
+import com.supcoder.hub.db.dao.UserMapper
+import com.supcoder.hub.db.domain.User
+import com.supcoder.hub.db.domain.UserExample
+import com.supcoder.hub.db.model.UserVo
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import javax.annotation.Resource
 
 @Service
 class UserService {
 
-    @Autowired
-    private lateinit var userRepository: UserRepository
+    @Resource
+    private lateinit var userMapper: UserMapper
 
-    fun findAllUsers(): List<User> {
-        return userRepository.findAll()
+
+    fun findById(userId: Long): User {
+        return userMapper.selectByPrimaryKey(userId);
     }
 
-    fun findUserById(id: Long): User? {
-        return userRepository.findById(id).orElse(null)
+    fun findUserVoById(userId: Long): UserVo {
+        val user = findById(userId)
+        return UserVo(
+            id = user.id,
+            username = user.username,
+            email = user.email,
+            password = user.password,
+            name = user.name,
+            avatar = user.avatar,
+            signature = user.signature,
+            title = user.title,
+            group = user.group,
+            notifyCount = user.notifyCount,
+            unreadCount = user.unreadCount,
+        )
     }
 
-    fun saveUser(user: User): User {
-        return userRepository.save(user)
+    fun queryByUsername(username: String): User? {
+        val userExample = UserExample()
+        userExample.or().andUsernameEqualTo(username)
+        return userMapper.selectOneByExample(userExample)
     }
 
-    fun deleteUserById(id: Long) {
-        userRepository.deleteById(id)
+    fun add(user: User) {
+        val now = LocalDateTime.now()
+        user.addTime = now
+        user.updateTime = now
+        userMapper.insertSelective(user)
     }
 
-    fun findByUsername(username: String): User? {
-        return userRepository.findByUsername(username)
+    fun updateById(user: User) {
+        user.updateTime = LocalDateTime.now()
+        userMapper.updateByPrimaryKeySelective(user)
     }
+
+    fun count(): Long {
+        val userExample = UserExample()
+        userExample.or().andDeletedEqualTo(false)
+        return userMapper.countByExample(userExample)
+    }
+
+    fun deleteById(userId: Long) {
+        userMapper.logicalDeleteByPrimaryKey(userId)
+    }
+
+    fun checkUsername(username: String): Boolean {
+        val userExample = UserExample()
+        userExample.or().andUsernameEqualTo(username)
+        return userMapper.countByExample(userExample).toInt() != 0
+    }
+
+    fun queryListByUsername(username: String): List<User> {
+        val userExample = UserExample()
+        userExample.or().andUsernameEqualTo(username)
+        return userMapper.selectByExample(userExample)
+    }
+
+
+    fun querySelective(
+        username: String?,
+        page: Int,
+        size: Int,
+        sort: String,
+        order: String
+    ): List<User> {
+        val userExample = UserExample()
+        val criteria = userExample.createCriteria()
+        if (!StringUtils.isEmpty(username)) {
+            criteria.andUsernameLike("%$username%")
+        }
+        criteria.andDeletedEqualTo(false)
+        if (!StringUtils.isEmpty(sort) && !StringUtils.isEmpty(order)) {
+            userExample.setOrderByClause("$sort $order")
+        }
+        PageHelper.startPage<User>(page, size)
+        return userMapper.selectByExample(userExample)
+    }
+
+
 }
 
