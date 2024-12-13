ad09c7632b07d02f894c06f8562594c8dc569d88
lee <noble.lee@foxmail.com>
2024-12-13 15:55:08 +0800

    feat(gateway): 添加网关鉴权、验证码、限流等功能- 新增 AuthFilter 实现网关鉴权功能 - 添加 BlackListUrlFilter 实现黑名单过滤- 新增 CacheRequestFilter 解决请求体缓存问题 - 添加 CaptchaConfig 配置验证码生成 - 新增 GatewayConfig 配置网关限流 - 实现 GatewayExceptionHandler 统一异常处理 - 添加 SpringDocConfig 配置 Swagger 文档 - 新增 ValidateCodeFilter 实现验证码验证- 添加 ValidateCodeHandler 处理验证码生成请求 - 实现 ValidateCodeService 接口及其实现类 - 新增 XssFilter 实现跨站脚本过滤

--- supcoder-gateway/src/main/java/com/supcoder/gateway/SupcoderGatewayApplication.java
+++ supcoder-gateway/src/main/java/com/supcoder/gateway/SupcoderGatewayApplication.java
@@ -2,12 +2,14 @@ package com.supcoder.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SupcoderGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupcoderGatewayApplication.class, args);
+		System.out.println("(♥◠‿◠)ﾉﾞ  若依网关启动成功   ლ(´ڡ`ლ)ﾞ  \n");
 	}
 
 }
---
+++ supcoder-gateway/src/main/java/com/supcoder/gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.supcoder.gateway.config;
+
+import com.supcoder.gateway.handler.SentinelFallbackHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+/**
+ * 网关限流配置
+ * 
+ * @author supcoder
+ */
+@Configuration
+public class GatewayConfig
+{
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelFallbackHandler sentinelGatewayExceptionHandler()
+    {
+        return new SentinelFallbackHandler();
+    }
+}
