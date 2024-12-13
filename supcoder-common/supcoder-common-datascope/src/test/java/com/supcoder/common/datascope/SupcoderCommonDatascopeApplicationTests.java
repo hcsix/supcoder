1a4a5077ba492aad9bf2a3711ddd3383801a993e
lee <noble.lee@foxmail.com>
2024-12-13 17:11:17 +0800

    feat(common): 添加数据权限和多数据源模块
    - 新增数据权限过滤注解和切面类，实现数据权限控制
    - 添加主从数据源注解，支持多数据源配置
    - 创建数据权限和数据源模块的pom文件，定义依赖
    -增加单元测试类，验证模块功能

---
+++ supcoder-common/supcoder-common-datascope/src/test/java/com/supcoder/common/datascope/SupcoderCommonDatascopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supcoder.common.datascope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupcoderCommonDatascopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
